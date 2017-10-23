8fc2910b97ab63f89a36ec2beba4ecc295545f78
Algore <teamxskill@gmail.com>
2017-10-22 20:38:41 -0700

    Add files via upload

---
+++ addons/pointshop-master/lua/pointshop/items/playermodels/niko.lua
@@ -0,0 +1,22 @@
+ITEM.Name = 'Niko Belic'
+ITEM.Price = 10000
+ITEM.Model = 'models/player/niko.mdl'
+ITEM.AllowedUserGroups = { "admin", "vip", "supervip" }
+
+function ITEM:OnEquip(ply, modifications)
+	if not ply._OldModel then
+		ply._OldModel = ply:GetModel()
+	end
+
+	timer.Simple(1, function() ply:SetModel(self.Model) end)
+end
+
+function ITEM:OnHolster(ply)
+	if ply._OldModel then
+		ply:SetModel(ply._OldModel)
+	end
+end
+
+function ITEM:PlayerSetModel(ply)
+	ply:SetModel(self.Model)
+end
---
+++ addons/pointshop-master/lua/pointshop/items/vipshop/__category.lua
@@ -0,0 +1,3 @@
+CATEGORY.Name = 'VIP Shop'
+CATEGORY.Icon = 'star'
+CATEGORY.AllowedUserGroups = { "founder","manager","communitymanager","headofstaff", "vipadmin", "vipmod", "supervip", "vip" }
