d407480aa766051447b1a9ae8d276fccd4c78a57
Algore <teamxskill@gmail.com>
2017-10-29 11:05:09 -0700

    Update sv_level_system.lua

--- addons/sharps_level_system/lua/autorun/sv_level_system.lua
+++ addons/sharps_level_system/lua/autorun/sv_level_system.lua
@@ -24,21 +24,72 @@ function SavePlayerData(ply)
 end
 hook.Add("PlayerDisconnected", "SavePlayerData", SavePlayerData)
 
-local time = 0
-hook.Add("Think", "LevelThink", function()
-	if (CurTime() < time) then return end
+function InitializePlayerData(ply)
+	// Check if the PData doesn't exist
+	if (ply:GetPData("Level") == nil) then
+		// Create the PData and NWInt
+		ply:SetPData("Level", 1)
+		ply:SetNWInt("Level", 1)
+		// Create the PData and NWInt
+		ply:SetPData("XP", 0)
+		ply:SetNWInt("XP", 0)
+	// Else, if it does exist..
+	else
+		// Just create the NWInt
+		ply:SetNWInt("Level", ply:GetPData("Level"))
+		ply:SetNWInt("XP", ply:GetPData("XP"))
+	end
+end
+hook.Add("PlayerInitialSpawn", "InitializePlayerData", InitializePlayerData)
+function SavePlayerData(ply)
+	// Update the player's level and xp PData
+	ply:SetPData("Level", ply:GetNWInt("Level"))
+	ply:SetPData("XP", ply:GetNWInt("XP"))
+end
+hook.Add("PlayerDisconnected", "SavePlayerData", SavePlayerData)
 
-	for k, v in pairs(player.GetAll()) do
-		if (v:GetLevel() >= 65) then return end
-		v:AddXP(100)
-
-		local NeededXP = v:GetMaxXP()
-
-		if (v:GetXP() >= NeededXP) then
-			v:SetXP(v:GetXP() - NeededXP)
-			v:LevelUp()
+local function TTTWinRoundPoints(result)
+--	if result == WIN_TIMELIMIT then return end 
+	if result == WIN_INNOCENT then 
+		for _,ply in pairs(player.GetAll() ) do
+			if (ply:GetRole() == ROLE_INNOCENT or ply:GetRole()== ROLE_DETECTIVE) then
+				if ply:Alive() then
+					ply:AddXP( 10 )
+					ply:ChatPrint("+10XP for winning and surviving as innocent")
+				elseif not ply:Alive() then 
+					ply:AddXP( 5 )
+					ply:ChatPrint("+5XP for winning but dying as innocent")
+				end
+			elseif ply:GetRole() == TEAM_SPEC then return
+			end
+		end
+	elseif result == WIN_TRAITOR then
+		for _,ply in pairs(player.GetAll() ) do
+			if ply:GetRole() == ROLE_TRAITOR then
+				if ply:Alive() then
+					ply:AddXP( 25 )
+					ply:ChatPrint("+25XP for winning and surviving as traitor")
+				elseif not ply:Alive() then
+					ply:AddXP( 10 )
+					ply:ChatPrint("+10XP for winning and dying as traitor")
+				end
+			elseif ply:GetRole() == TEAM_SPEC then return
+			end
+		end
+	elseif result == WIN_TIMELIMIT then 
+		for _,ply in pairs(player.GetAll() ) do
+			if (ply:GetRole() == ROLE_INNOCENT or ply:GetRole()== ROLE_DETECTIVE) then
+				if ply:Alive() then
+					ply:AddXP( 5 )
+					ply:ChatPrint("+5XP because the traitors are being puss bois")
+				elseif not ply:Alive() then 
+					ply:AddXP( 5 )
+					ply:ChatPrint("+5XP because the traitors are being puss bois")
+				end
+			end
 		end
 	end
+end -- end func
+hook.Add("TTTEndRound", "WinPoints", TTTWinRoundPoints)
 
-	time = 1 + CurTime()
-end)
+
