12375205be45ae001a1f0a1c582f03c13d61fdd0
Algore <teamxskill@gmail.com>
2017-10-29 10:48:13 -0700

    Update tttwin.lua

--- addons/sharps_level_system/lua/autorun/tttwin.lua
+++ addons/sharps_level_system/lua/autorun/tttwin.lua
@@ -4,10 +4,10 @@ local function TTTWinRoundPoints(result)
 		for _,ply in pairs(player.GetAll() ) do
 			if (ply:GetRole() == ROLE_INNOCENT or ply:GetRole()== ROLE_DETECTIVE) then
 				if ply:Alive() then
-					ply:StatsAddXp( 10 )
+					ply:AddXP( 10 )
 					ply:ChatPrint("+10XP for winning and surviving as innocent")
 				elseif not ply:Alive() then 
-					ply:StatsAddXp( 5 )
+					ply:AddXP( 5 )
 					ply:ChatPrint("+5XP for winning but dying as innocent")
 				end
 			elseif ply:GetRole() == TEAM_SPEC then return
@@ -17,10 +17,10 @@ local function TTTWinRoundPoints(result)
 		for _,ply in pairs(player.GetAll() ) do
 			if ply:GetRole() == ROLE_TRAITOR then
 				if ply:Alive() then
-					ply:StatsAddXp( 25 )
+					ply:AddXP( 25 )
 					ply:ChatPrint("+25XP for winning and surviving as traitor")
 				elseif not ply:Alive() then
-					ply:StatsAddXp( 10 )
+					ply:AddXP( 10 )
 					ply:ChatPrint("+10XP for winning and dying as traitor")
 				end
 			elseif ply:GetRole() == TEAM_SPEC then return
@@ -30,10 +30,10 @@ local function TTTWinRoundPoints(result)
 		for _,ply in pairs(player.GetAll() ) do
 			if (ply:GetRole() == ROLE_INNOCENT or ply:GetRole()== ROLE_DETECTIVE) then
 				if ply:Alive() then
-					ply:StatsAddXp( 5 )
+					ply:AddXP( 5 )
 					ply:ChatPrint("+5XP because the traitors are being puss bois")
 				elseif not ply:Alive() then 
-					ply:StatsAddXp( 5 )
+					ply:AddXP( 5 )
 					ply:ChatPrint("+5XP because the traitors are being puss bois")
 				end
 			end
