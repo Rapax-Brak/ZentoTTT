8da117992a23cca9baed357ec8bc7b26e4b0ba1f
Algore <teamxskill@gmail.com>
2017-10-31 17:16:42 -0700

    Update level_commands.lua

--- addons/sharps_level_system/lua/ulx/modules/sh/level_commands.lua
+++ addons/sharps_level_system/lua/ulx/modules/sh/level_commands.lua
@@ -1,4 +1,4 @@
-local CATEGORY_NAME = "Level System"
+local CATEGORY_NAME = "Level"
 
 // Concommands
 local function findPlayer(info)
@@ -26,39 +26,39 @@ end
 /*
 	Add XP
 */
-function ulx.add_xp(calling_ply, target_ply, amount)
+function ulx.addxp(calling_ply, target_ply, amount)
 	if (!target_ply) then print("Invalid target!") return end
 	target_ply:AddXP(amount)
 
 	ulx.fancyLogAdmin(calling_ply, "#A added #s xp to #T", target_ply, amount)
 end
 
-local add_xp = ulx.command(CATEGORY_NAME, "ulx add_xp", ulx.add_xp, "!add_xp", true)
-add_xp:addParam{ type=ULib.cmds.PlayerArg }
-add_xp:addParam{ type=ULib.cmds.StringArg }
-add_xp:defaultAccess(ULib.ACCESS_SUPERADMIN)
-add_xp:help("Adds XP to the specified player.")
+local addxp = ulx.command(CATEGORY_NAME, "ulx addxp", ulx.addxp, "!addxp", true)
+addxp:addParam{ type=ULib.cmds.PlayerArg }
+addxp:addParam{ type=ULib.cmds.StringArg }
+addxp:defaultAccess(ULib.ACCESS_SUPERADMIN)
+addxp:help("Adds XP to the specified player.")
 
 /*
 	Set XP
 */
-function ulx.set_xp(calling_ply, target_ply, amount)
+function ulx.setxp(calling_ply, target_ply, amount)
 	if (!target_ply) then print("Invalid target!") return end
 	target_ply:SetXP(amount)
 
 	ulx.fancyLogAdmin(calling_ply, "#A set #T's xp to #s", target_ply, amount)
 end
 
-local set_xp = ulx.command(CATEGORY_NAME, "ulx set_xp", ulx.set_xp, "!set_xp", true)
-set_xp:addParam{ type=ULib.cmds.PlayerArg }
-set_xp:addParam{ type=ULib.cmds.StringArg }
-set_xp:defaultAccess(ULib.ACCESS_SUPERADMIN)
-set_xp:help("Sets the XP of the specified player.")
+local setxp = ulx.command(CATEGORY_NAME, "ulx setxp", ulx.setxp, "!setxp", true)
+setxp:addParam{ type=ULib.cmds.PlayerArg }
+setxp:addParam{ type=ULib.cmds.StringArg }
+setxp:defaultAccess(ULib.ACCESS_SUPERADMIN)
+setxp:help("Sets the XP of the specified player.")
 
 /*
 	Set Level
 */
-function ulx.set_level(calling_ply, target_ply, level)
+function ulx.setlevel(calling_ply, target_ply, level)
 	if (!target_ply) then print("Invalid target!") return end
 	target_ply:SetXP(0)
 	target_ply:SetLevel(level)
@@ -66,23 +66,23 @@ function ulx.set_level(calling_ply, target_ply, level)
 	ulx.fancyLogAdmin(calling_ply, "#A set #T's level to #s", target_ply, level)
 end
 
-local set_level = ulx.command(CATEGORY_NAME, "ulx set_level", ulx.set_level, "!set_level", true)
-set_level:addParam{ type=ULib.cmds.PlayerArg }
-set_level:addParam{ type=ULib.cmds.StringArg }
-set_level:defaultAccess(ULib.ACCESS_SUPERADMIN)
-set_level:help("Sets the Level of the specified player.")
+local setlevel = ulx.command(CATEGORY_NAME, "ulx setlevel", ulx.setlevel, "!setlevel", true)
+setlevel:addParam{ type=ULib.cmds.PlayerArg }
+setlevel:addParam{ type=ULib.cmds.StringArg }
+setlevel:defaultAccess(ULib.ACCESS_SUPERADMIN)
+setlevel:help("Sets the Level of the specified player.")
 
 /*
 	Level Up
 */
-function ulx.level_up(calling_ply, target_ply)
+function ulx.levelup(calling_ply, target_ply)
 	if (!target_ply) then print("Invalid target!") return end
 	target_ply:LevelUp()
 
 	ulx.fancyLogAdmin(calling_ply, "#A leveled up #T", target_ply)
 end
 
-local level_up = ulx.command(CATEGORY_NAME, "ulx level_up", ulx.level_up, "!level_up", true)
-level_up:addParam{ type=ULib.cmds.PlayerArg }
-level_up:defaultAccess(ULib.ACCESS_SUPERADMIN)
-level_up:help("Levels up the specified player.")
+local levelup = ulx.command(CATEGORY_NAME, "ulx levelup", ulx.levelup, "!levelup", true)
+levelup:addParam{ type=ULib.cmds.PlayerArg }
+levelup:defaultAccess(ULib.ACCESS_SUPERADMIN)
+levelup:help("Levels up the specified player.")
